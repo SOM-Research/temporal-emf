f66388b4f143fbd4f5a57c42f5704ceda990b683
Abel Gómez <agomezlla@uoc.edu>
2020-11-25 06:20:56 +0100

    Assert that files are actually generated, and silence console

--- tests/edu.uoc.som.temf.generator.tests/src/edu/uoc/som/temf/generator/tests/GenerateModelTest.java
+++ tests/edu.uoc.som.temf.generator.tests/src/edu/uoc/som/temf/generator/tests/GenerateModelTest.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.emf.codegen.ecore.generator.Generator;
 import org.eclipse.emf.codegen.ecore.genmodel.GenDelegationKind;
 import org.eclipse.emf.codegen.ecore.genmodel.GenJDKLevel;
@@ -34,7 +36,6 @@ class GenerateModelTest {
 	private static final String TEST_MODEL_PLUGIN_ID = TestmodelPlugin.getPlugin().getSymbolicName();
 	private static final String TEST_MODEL_PATH = "/model/testmodel.ecore";
 
-
 	@Test
 	void testMigrateGenmodel() throws IOException {
 		GenModel genModel = createGenModel();
@@ -59,19 +60,44 @@ class GenerateModelTest {
 	@Test
 	void testGenerateModelCode() throws Exception {
 		GenModel genModel = createGenModel();
-		Resource genModelResource = new XMIResourceImpl(URI.createPlatformResourceURI(TEST_MODEL_PLUGIN_ID + TEST_MODEL_PATH, true).trimFileExtension().appendFileExtension("genmodel"));
+		Resource genModelResource = new XMIResourceImpl(
+				URI.createPlatformResourceURI(TEST_MODEL_PLUGIN_ID + TEST_MODEL_PATH, true).trimFileExtension().appendFileExtension("genmodel"));
 		genModelResource.getContents().add(genModel);
 
 		Generator generator = GenModelUtil.createGenerator(genModel);
-		generator.generate(genModel, GenBaseGeneratorAdapter.MODEL_PROJECT_TYPE, new BasicMonitor.Printing(System.out));
-		
+		// In case of error, new BasicMonitor.Printing(System.out) maybe used instead of
+		// new BasicMonitor()
+		generator.generate(genModel, GenBaseGeneratorAdapter.MODEL_PROJECT_TYPE, new BasicMonitor());
+
+		IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(TEST_MODEL_PLUGIN_ID);
+
+		// @formatter:off
+		assertAll(
+				() -> assertTrue(project.findMember("/.classpath").exists()),
+				() -> assertTrue(project.findMember("/.project").exists()),
+				() -> assertTrue(project.findMember("/build.properties").exists()),
+				() -> assertTrue(project.findMember("/plugin.properties").exists()),
+				() -> assertTrue(project.findMember("/plugin.xml").exists()),
+				() -> assertTrue(project.findMember("/META-INF/MANIFEST.MF").exists()),
+				() -> assertTrue(project.findMember("/src/edu/uoc/som/temf/testmodel/impl/NodeImpl.java").exists()),
+				() -> assertTrue(project.findMember("/src/edu/uoc/som/temf/testmodel/impl/TestmodelFactoryImpl.java").exists()),
+				() -> assertTrue(project.findMember("/src/edu/uoc/som/temf/testmodel/impl/TestmodelPackageImpl.java").exists()),
+				() -> assertTrue(project.findMember("/src/edu/uoc/som/temf/testmodel/Node.java").exists()),
+				() -> assertTrue(project.findMember("/src/edu/uoc/som/temf/testmodel/TestmodelFactory.java").exists()),
+				() -> assertTrue(project.findMember("/src/edu/uoc/som/temf/testmodel/TestmodelPackage.java").exists()),
+				() -> assertTrue(project.findMember("/src/edu/uoc/som/temf/testmodel/TestmodelPlugin.java").exists()),
+				() -> assertTrue(project.findMember("/src/edu/uoc/som/temf/testmodel/util/TestmodelAdapterFactory.java").exists()),
+				() -> assertTrue(project.findMember("/src/edu/uoc/som/temf/testmodel/util/TestmodelSwitch.java").exists())
+		);
+		// @formatter:on
+
 		genModelResource.save(Collections.emptyMap());
 	}
 
 	private static GenModel createGenModel() throws IOException {
 		Resource resource = new XMIResourceImpl(URI.createPlatformPluginURI(TEST_MODEL_PLUGIN_ID + TEST_MODEL_PATH, true));
 		resource.load(Collections.emptyMap());
-		
+
 		GenModel genModel = GenModelFactory.eINSTANCE.createGenModel();
 		genModel.initialize(Arrays.asList((EPackage) resource.getContents().get(0)));
 		TEmfMigratorUtil.adjustGenModel(genModel);
@@ -83,7 +109,7 @@ class GenerateModelTest {
 		genModel.setModelPluginID(TEST_MODEL_PLUGIN_ID);
 		genModel.setModelDirectory("/" + TEST_MODEL_PLUGIN_ID + "/src");
 		genModel.setCanGenerate(true);
-		
+
 		genPackage.setBasePackage(TEST_MODEL_PLUGIN_ID.substring(0, TEST_MODEL_PLUGIN_ID.lastIndexOf(".")));
 		return genModel;
 	}
